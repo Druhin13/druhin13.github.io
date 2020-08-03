d1a19274997c45a15ba4f79230260bbf38cb51e2
Druhin Tarafder <druhinpro@gmail.com>
2020-08-03 10:33:13 +0530

    Add files via upload

--- heading.js
+++ heading.js
@@ -1,23 +1,61 @@
 // Wrap every letter in a span
-var textWrapper = document.querySelector('.ml7 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+			var textWrapper = document.querySelector('.ml7 .letters');
+			textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-anime.timeline({
-		loop: true
-	})
-	.add({
-		targets: '.ml7 .letter',
-		translateY: ["1.1em", 0],
-		translateX: ["0.55em", 0],
-		translateZ: 0,
-		rotateZ: [180, 0],
-		duration: 750,
-		easing: "easeOutExpo",
-		delay: (el, i) => 50 * i
-	}).add({
-		targets: '.ml7',
-		opacity: 0,
-		duration: 1000,
-		easing: "easeOutExpo",
-		delay: 1000
-	});
+			anime.timeline({
+					loop: false
+				})
+				.add({
+					targets: '.ml7 .letter',
+					translateY: ["1.1em", 0],
+					translateX: ["0.55em", 0],
+					translateZ: 0,
+					rotateZ: [180, 0],
+					duration: 500,
+					easing: "easeOutExpo",
+					delay: (el, i) => 50 * i
+				});
+			
+			
+			//********************************************//
+			
+			
+			// Wrap every letter in a span
+			var textWrapper = document.querySelector('.ml7 .letters2');
+			textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+			anime.timeline({
+					loop: false
+				})
+				.add({
+					targets: '.ml7 .letter',
+					translateY: ["1.1em", 0],
+					translateX: ["0.55em", 0],
+					translateZ: 0,
+					rotateZ: [180, 0],
+					duration: 700,
+					easing: "easeOutExpo",
+					delay: (el, i) => 50 * i
+				});
+			
+			
+			//********************************************//
+			
+			
+			// Wrap every letter in a span
+			var textWrapper = document.querySelector('.ml7 .letters3');
+			textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+			anime.timeline({
+					loop: false
+				})
+				.add({
+					targets: '.ml7 .letter',
+					translateY: ["1.1em", 0],
+					translateX: ["0.55em", 0],
+					translateZ: 0,
+					rotateZ: [180, 0],
+					duration: 900,
+					easing: "easeOutExpo",
+					delay: (el, i) => 50 * i
+				});
