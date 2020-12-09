718fff69527f87c5b35e4dfa4dceb013a1193f11
Druhin Tarafder <druhinpro@gmail.com>
2020-12-09 20:47:35 +0530

    Add files via upload

--- luxy.js
+++ luxy.js
@@ -1,12 +1,9 @@
-<script src="https://min30327.github.io/luxy.js/dist/js/luxy.js"></script>
+//	<script charset="utf-8">
+var isMobile = /iPhone|iPad|Android/i.test(navigator.userAgent);
+if (!isMobile) {
+	luxy.init({
+		wrapper: '#luxy',
+		wrapperSpeed: 0.065,
 
-	<script charset="utf-8">
-		var isMobile = /iPhone|iPad|Android/i.test(navigator.userAgent);
-		if (!isMobile) {
-			luxy.init({
-				wrapper: '#luxy',
-				wrapperSpeed: 0.065,
-
-			});
-		}
-	</script>
+	});
+}
