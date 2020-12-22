621955b458c9544c24ec5c29d5fe840a948a4ec0
Druhin Tarafder <druhinpro@gmail.com>
2020-12-22 21:27:20 +0530

    Add files via upload

---
+++ cursor.js
@@ -0,0 +1,86 @@
+let tricksCursor = document.querySelector('.cursor');
+window.addEventListener('mousemove', cursor);
+
+function cursor(e) {
+	tricksCursor.style.top = e.pageY + 'px';
+	tricksCursor.style.left = e.pageX + 'px';
+}
+
+$(".menuitems").mouseenter(function () {
+	window.removeEventListener("mousemove", cursor);
+	var tricksWidth = $(this).outerWidth() / 2;
+	var tricksHeight = $(this).outerHeight() / 2;
+	var tricksTop = $(this).offset().top;
+	var tricksLeft = $(this).offset().left;
+	tricksCursor.style.top = (tricksTop + tricksHeight) + 'px';
+	tricksCursor.style.left = (tricksLeft + tricksWidth) + 'px';
+});
+
+$(".menuitems").mouseleave(function () {
+	window.addEventListener("mousemove", cursor);
+});
+
+
+
+
+$(".link-block-6").mouseenter(function () {
+	window.removeEventListener("mousemove", cursor);
+	var tricksWidth = $(this).outerWidth() / 2;
+	var tricksHeight = $(this).outerHeight() / 2;
+	var tricksTop = $(this).offset().top;
+	var tricksLeft = $(this).offset().left;
+	tricksCursor.style.top = (tricksTop + tricksHeight) + 'px';
+	tricksCursor.style.left = (tricksLeft + tricksWidth) + 'px';
+});
+
+$(".link-block-6").mouseleave(function () {
+	window.addEventListener("mousemove", cursor);
+});
+
+
+
+
+
+// Add class on hover
+$("a").mouseenter(function () {
+	$('.cursor').addClass('cursor-hover');
+});
+
+$("a").mouseleave(function () {
+	$('.cursor').removeClass('cursor-hover');
+});
+
+
+
+
+// Add class on hover of bigname
+$(".div-block-20").mouseenter(function () {
+	$('.cursor').addClass('bigname-hover');
+});
+
+$(".div-block-20").mouseleave(function () {
+	$('.cursor').removeClass('bigname-hover');
+});
+
+
+
+// Add class on hover of video
+$(".background-video-2").mouseenter(function () {
+	$('.cursor').addClass('video-hover');
+});
+
+$(".background-video-2").mouseleave(function () {
+	$('.cursor').removeClass('video-hover');
+});
+
+
+
+
+// Add class on mouse down
+$("body").mousedown(function () {
+	$('.cursor').addClass('cursor-pressed');
+});
+
+$("body").mouseup(function () {
+	$('.cursor').removeClass('cursor-pressed');
+});
