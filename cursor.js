12051c297c5735205eeb999030d76c4cb0a705ad
Druhin Tarafder <druhinpro@gmail.com>
2020-12-23 19:20:16 +0530

    Add files via upload

--- cursor.js
+++ cursor.js
@@ -76,6 +76,100 @@ $(".background-video-2").mouseleave(function () {
 
 
 
+
+// Add class on hover of coursework
+$(".cs1").mouseenter(function () {
+	$('.cursor').addClass('cs-hover');
+});
+
+$(".cs1").mouseleave(function () {
+	$('.cursor').removeClass('cs-hover');
+});
+
+// Add class on hover of coursework
+$(".cs2").mouseenter(function () {
+	$('.cursor').addClass('cs-hover');
+});
+
+$(".cs2").mouseleave(function () {
+	$('.cursor').removeClass('cs-hover');
+});
+
+// Add class on hover of coursework
+$(".cs3").mouseenter(function () {
+	$('.cursor').addClass('cs-hover');
+});
+
+$(".cs3").mouseleave(function () {
+	$('.cursor').removeClass('cs-hover');
+});
+
+// Add class on hover of coursework
+$(".cs4").mouseenter(function () {
+	$('.cursor').addClass('cs-hover');
+});
+
+$(".cs4").mouseleave(function () {
+	$('.cursor').removeClass('cs-hover');
+});
+
+
+
+
+
+// Add class on hover of button
+$(".cw-button").mouseenter(function () {
+	$('.cursor').addClass('button-hover');
+});
+
+$(".cw-button").mouseleave(function () {
+	$('.cursor').removeClass('button-hover');
+});
+
+
+
+
+// Add class on hover of button
+$(".resume-button").mouseenter(function () {
+	$('.cursor').addClass('button-hover');
+});
+
+$(".resume-button").mouseleave(function () {
+	$('.cursor').removeClass('button-hover');
+});
+
+
+
+
+// Add class on hover of button
+$(".contact-button").mouseenter(function () {
+	$('.cursor').addClass('button-hover');
+});
+
+$(".contact-button").mouseleave(function () {
+	$('.cursor').removeClass('button-hover');
+});
+
+
+
+
+// Add class on hover of button
+$(".cw-img").mouseenter(function () {
+	$('.cursor').addClass('cw-hover');
+});
+
+$(".cw-img").mouseleave(function () {
+	$('.cursor').removeClass('cw-hover');
+});
+
+
+
+
+
+
+
+
+
 // Add class on mouse down
 $("body").mousedown(function () {
 	$('.cursor').addClass('cursor-pressed');
